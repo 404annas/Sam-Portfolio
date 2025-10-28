f1d222d64c6333e94c4d76f8455943355eb1dda0
Muhammad Annas Khan <annasking601@gmail.com>
2025-10-29 01:49:03 +0500

    Responsive 7

--- src/components/CardWrapper.jsx
+++ src/components/CardWrapper.jsx
@@ -6,7 +6,7 @@ const CardWrapper = ({ children }) => {
       className="
         bg-first noise-on-card
         w-[85vw] sm:w-[70vw] md:w-[55vw] lg:w-[40vw] xl:w-[28vw]
-        h-full sm:h-[75vh] md:h-[80vh] lg:h-[85vh] xl:h-[85vh]
+        h-fit sm:h-[75vh] md:h-[80vh] lg:h-[85vh] xl:h-[85vh]
         rounded-2xl flex flex-col overflow-hidden mx-auto
       "
     >
